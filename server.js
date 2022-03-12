4b1fa376bd061f6249482925760f2ee17901d797
dpmurphy11 <dpmurphy_onsite@hotmail.com>
2022-03-12 07:12:24 -0500

    heroku

--- server.js
+++ server.js
@@ -18,4 +18,5 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.
 // fallback route
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.listen(PORT);
+app.listen(PORT, "0.0.0.0", () => console.log("Listening on Port 3000"));
+// app.listen(PORT);
