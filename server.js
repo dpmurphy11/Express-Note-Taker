b88ccc2290c9b77e3f825f3d67c9f20ba5f8a6d2
dpmurphy11 <dpmurphy_onsite@hotmail.com>
2022-03-12 06:48:38 -0500

    heroku

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 // fallback route
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT);
